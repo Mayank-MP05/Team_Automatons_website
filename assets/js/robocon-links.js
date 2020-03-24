df57da2612506ee6c41b3a6c19a2e562d64ce9d5
Mayank_MP5 <mayank5pande@gmail.com>
2020-03-24 17:41:39 +0530

    Code for Image Placement Added

--- assets/js/robocon-links.js
+++ assets/js/robocon-links.js
@@ -1,9 +1,10 @@
  
     const img = document.getElementById("base-img");
+
     img.onload = () => {
     const len = img.width;
     const ht = img.height;
-
+    console.log(len,ht)
     const arr = [
         {
             left : 108,
@@ -23,17 +24,20 @@
             top : 56
         }
     ]
+    const btnHt = 100 * len / 849;
+    const btnWd = 100 * ht / 539;
     for(let i = 0 ; i < 5 ; i++){
         let bbtn = document.getElementById(`b${i+1}`);
         let l = arr[i].left * len / 849;
         let t = arr[i].top * ht / 539;
-        let btnHt = 100 * len / 849;
-        let btnWd = 100 * ht / 539;
+
         bbtn.style.left = `${l}px`
+        console.log(t)
         bbtn.style.top = `${t}px`
         bbtn.style.height = `${btnHt}px`
+        console.log(btnWd)
         bbtn.style.width = `${btnWd}px`
     }
-    const r2 = document.getElementById("p-top-row")
+    const r2 = document.getElementById("footer-row")
     r2.style.marginTop = `${ht + 20}px`
-    }
+}
