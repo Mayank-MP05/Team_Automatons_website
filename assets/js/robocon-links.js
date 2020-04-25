e7f7a6ede019be70aee67c08873fc0b4e373b7c2
Mayank_MP5 <mayank5pande@gmail.com>
2020-04-25 17:29:19 +0530

    Last one

--- assets/js/robocon-links.js
+++ assets/js/robocon-links.js
@@ -1,4 +1,4 @@
- 
+
     const img = document.getElementById("base-img");
 
     img.onload = () => {
@@ -41,3 +41,47 @@
     const r2 = document.getElementById("footer-row")
     r2.style.marginTop = `${ht + 20}px`
 }
+
+const reload = () => {
+	const len = img.width;
+    const ht = img.height;
+    console.log(len,ht)
+    const arr = [
+        {
+            left : 108,
+            top : 310
+        },
+        {
+            left : 489,
+            top : 310
+        },{
+            left : 201,
+            top : 156
+        },{
+            left : 470,
+            top : 160
+        },{
+            left : 687,
+            top : 56
+        }
+    ]
+    const btnHt = 100 * len / 849;
+    const btnWd = 100 * ht / 539;
+    for(let i = 0 ; i < 5 ; i++){
+        let bbtn = document.getElementById(`b${i+1}`);
+        let l = arr[i].left * len / 849;
+        let t = arr[i].top * ht / 539;
+
+        bbtn.style.left = `${l}px`
+        console.log(t)
+        bbtn.style.top = `${t}px`
+        bbtn.style.height = `${btnHt}px`
+        console.log(btnWd)
+        bbtn.style.width = `${btnWd}px`
+    }
+    const r2 = document.getElementById("footer-row")
+    r2.style.marginTop = `${ht + 20}px`
+}
+
+ for(let i = 0 ; i < 10 ; i++)
+      setTimeout(reload, 300);
